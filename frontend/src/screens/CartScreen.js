845c14cfd3476de6df064a26ae50d03d347f0a69
Facundo Troitero <ftroitero@gmail.com>
2021-06-11 17:07:04 +0100

    built shopping screen anc compoents

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,22 @@
 import "./CartScreen.css";
+import CartItem from "../components/CartItem";
 
 const CartScreen = () => {
-	return <div>CartScreen</div>;
+	return (
+		<div className="cartscreen">
+			<div className="cartscreen__left">
+				<h2>Shopping Cart</h2>
+				<CartItem />
+			</div>
+			<div className="cartscreen__right">
+				<div className="cartscreen__info">
+					<p>Subtotal (0) items</p>
+					<p>$500</p>
+					<button>Proceed To Checkout</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CartScreen;
