516e3cc1e6b3474da03063707e9b2242220a8e78
Facundo Troitero <ftroitero@gmail.com>
2021-06-13 13:32:57 +0100

    navbar font change

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,6 +2,8 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+const colors = ["#0bf", "#f0b", "#fb0", "#b0f"];
+
 const Navbar = ({ click }) => {
 	const cart = useSelector((state) => state.cart);
 
@@ -15,7 +17,13 @@ const Navbar = ({ click }) => {
 		<nav className="navbar">
 			<div className="navbar__logo">
 				<Link to="/">
-					<h2>MERN Shopping Cart</h2>
+					<h2>
+						<span style={{ color: "#539E45" }}>M</span>
+						<span style={{ color: "#C37E75" }}>E</span>
+						<span style={{ color: "#71D4EC" }}>R</span>
+						<span style={{ color: "#98C049" }}>N</span>
+						<span> Shopping Cart</span>
+					</h2>
 				</Link>
 			</div>
 
