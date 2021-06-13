a08fac6f70079311941e25db76cb402140f8e63a
Facundo Troitero <ftroitero@gmail.com>
2021-06-13 14:39:05 +0100

    deploy config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const path = require("path");
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
@@ -13,11 +14,23 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-	res.json({ message: "API running..." });
-});
+// app.get("/", (req, res) => {
+// 	res.json({ message: "API running..." });
+// });
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+	});
+} else {
+	app.get("/", (req, res) => {
+		res.send("Api running");
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
