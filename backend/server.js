283efceab6be9dc09c7e9f5a2f3aa56862bde2e6
Facundo Troitero <ftroitero@gmail.com>
2021-06-13 12:24:46 +0100

    built redux folders and files

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
-const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 connectDB();
 
@@ -10,8 +11,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.json({ message: "API running..." });
+});
+
 app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
